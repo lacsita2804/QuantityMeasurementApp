7883c2614552936a0996941e6f09428de01b7d90
K.A Lacsita <kalac@example.com>
2026-04-27 09:31:56 +0530

    Implemented UC2 Feet and Inches equality

---
+++ QuantityMeasurementUC2.java
@@ -0,0 +1,47 @@
+// QuantityMeasurementApp.java
+
+public class QuantityMeasurementApp {
+
+    static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Feet feet = (Feet) obj;
+            return Double.compare(value, feet.value) == 0;
+        }
+    }
+
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches inch = (Inches) obj;
+            return Double.compare(value, inch.value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+
+        System.out.println("Feet Equal: " + f1.equals(f2));
+        System.out.println("Inches Equal: " + i1.equals(i2));
+    }
+}
